e48b72c6642c4a573e950f029c703c89383f71ad
sonsuke <bay0211@icloud.com>
2018-12-27 21:08:51 +0900

    test

--- index.php
+++ index.php
@@ -1,51 +1,72 @@
 <?php
 
+//Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 
-/*
-* 各キーを環境変数に設定
-*
-* Line Developers
-* CHANNEL_SECRET = Channel Secret
-* CHANNEL_ACCESS_TOKEN = Channel Access Token
-*
-*/
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
-// Line Message APIに接続
-$input = file_get_contents('php://input');
-$json = json_decode($input);
-$event = $json->events[0];
-$http_client = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new \LINE\LINEBot($http_client, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
-// メッセージ識別子を取得
-$event_type = $event->type;
-$event_message_type = $event->message->type;
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-// メッセージの場合
-if ('message' == $event_type) {
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+// 不正であれば例外の内容を出力
+try{
+  $events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 
-    // テキストメッセージの場合
-    if ('text' == $event_message_type) {
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException =>'.var_export($e, true));
 
-        // メッセージの取得
-        $text = $event->message->text;
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException =>'.var_export($e, true));
 
-        /*メッセージを受け取ったらメッセージをそのまま返す
-        $text_message_builder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
-        $response = $bot->replyMessage($event->replyToken, $text_message_builder);*/
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log('parseEventRequest failed. UnknownMessageTypeException =>'.var_export($e, true));
 
-    　  //画像を返信
-        replyImageMessage($bot,$event->getReplyToken(),'https://'.$_SERVER['HTTP_HOST'].'/imgs/original.jpg','https://'.$_SERVER['HTTP_HOST'].'/imgs/preview.jpg');
-    }
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException =>'.var_export($e, true));
 }
 
-function replyImageMessage($bot,$replyToken,$originalImageUrl,$previewImageUrl){
-  //ImageMessageBuilderの引数は画像URLとサムネイルURL
-  $response = $bot->replyMessage($replyToken,new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl,$previewImageUrl));
+//配列に格納された各イベントをループで処理
+foreach ((array)$events as $event){
+  // MessageEventクラスのインスタンスでなければ処理をスキップ
+  if(!($event instanceof \LINE\LINEBot\Event\MessageEvent)){
+    error_log('Non Message event has come');
+    continue;
+  }
+  // TextMessageBuilderクラスのインスタンスでなければ処理をスキップ
+  if(!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)){
+    error_log('Non Message event has come');
+    continue;
+  }
+  //オウム返し
+  $bot->replyText($event->getReplyToken(), $event->getText());
 
+}
+
+//テキストを返信。引数はLINEBot、返信先、テキスト
+function replyTextMessage($bot,$replyToken,$text) {
+  // 返信を行いメッセージを取得
+  // TextMessageBuilderの引数はテキスト
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+  //レスポンスが異常な場合
   if(!$response->isSucceeded()){
-    error_log('Failed!!!'.$response->getHTTPStatus.' '.$response->getRawBody());
+    //エラー内容を出力
+    error_log('Failed! '. $response->getHTTPStatus . ' '.$response->getRawBody());
   }
 }
+
+//画像を返信。引数はLINEBot、返信先、画像URL、サムネイルURL
+function replyImageMessage($bot,$replyToken,$originalImageUrl,$previewImageUrl){
+  // ImageMessageBuilderの引数は画像URL、サムネイルURL
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
+  if(!$response->isSucceeded()){
+    error_log('Failed! '. $response->getHTTPStatus . ' '.$response->getRawBody());
+  }
+}
+
 ?>
