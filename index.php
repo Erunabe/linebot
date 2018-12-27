69769352df70574b90c6d895c4b05c8add78befc
sonsuke <bay0211@icloud.com>
2018-12-27 21:39:06 +0900

    check

--- index.php
+++ index.php
@@ -43,11 +43,7 @@ foreach ((array)$events as $event){
     continue;
   }
     //オウム返し
-<<<<<<< HEAD
-    replyText($bot,$event->getReplyToken(), $event->getText());
-=======
-    replyText($event->getReplyToken(), $event->getText());
->>>>>>> f90035681e2ae6e40c4212b7b9068bed2ec6a395
+    replyTextMessage($bot,$event->getReplyToken(), $event->getText());
     //画像返信
     replyImageMessage($bot,$event->getReplyToken(),'https://'.$_SERVER['HTTP_HOST'].'/imgs/original.jpg','https://'.$_SERVER['HTTP_HOST'].'/imgs/preview.jpg');
 }
